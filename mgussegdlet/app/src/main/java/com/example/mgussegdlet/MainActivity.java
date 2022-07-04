9963bbe17298dadb17c5e458eb6daec9b0c56b6b
Kiss Csaba <66549421+csaba850211@users.noreply.github.com>
2022-07-04 14:29:20 +0200

    minden megyen zene video stb

--- mgussegdlet/app/src/main/java/com/example/mgussegdlet/MainActivity.java
+++ mgussegdlet/app/src/main/java/com/example/mgussegdlet/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebChromeClient(new WebChromeClient());
         webView.loadUrl("file:///android_asset/index.html");
-
-
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setMediaPlaybackRequiresUserGesture(false);
     }
 }
