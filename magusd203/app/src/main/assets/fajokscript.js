21be36d5dad8355da84e70063de65e6a61fb37a7
Kiss Csaba <66549421+csaba850211@users.noreply.github.com>
2022-07-11 17:03:38 +0200

    ember faj OK

--- magusd203/app/src/main/assets/fajokscript.js
+++ magusd203/app/src/main/assets/fajokscript.js
@@ -1,3 +1,53 @@
-function goIndex() {
-    location.href = "index.html";
+function menuselector() {
+    if (document.getElementById("menuselector").value == "home") {
+        window.location.href = 'index.html';
+    }
+
+    if (document.getElementById("menuselector").value == "fajok") {
+        window.location.href = 'fajok.html';
+    }
+
+    if (document.getElementById("menuselector").value == "kasztok") {
+        window.location.href = 'kasztok.html';
+    }
+
+    if (document.getElementById("menuselector").value == "kepessegek") {
+        window.location.href = 'kepessegek.html';
+    }
+
+    if (document.getElementById("menuselector").value == "kepzettsegek") {
+        window.location.href = 'kepzettsegek.html';
+    }
+
+    if (document.getElementById("menuselector").value == "pszi") {
+        window.location.href = 'pszi.html';
+    }
+
+    if (document.getElementById("menuselector").value == "varazslatok") {
+        window.location.href = 'varazslatok.html';
+    }
+
+    if (document.getElementById("menuselector").value == "papimagia") {
+        window.location.href = 'papimagia.html';
+    }
+
+    if (document.getElementById("menuselector").value == "satralis") {
+        window.location.href = 'satralis.html';
+    }
+
+    if (document.getElementById("menuselector").value == "istenek") {
+        window.location.href = 'istenek.html';
+    }
+
+    if (document.getElementById("menuselector").value == "kargen") {
+        window.location.href = 'kargen.html';
+    }
+    if (document.getElementById("menuselector").value == "sugo") {
+        window.location.href = 'sugo.html';
+    }
+
+}
+
+function jump() {
+    alert("szevasz");
 }
